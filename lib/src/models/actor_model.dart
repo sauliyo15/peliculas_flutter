dca3f6c68921e0abc8f00ee1b9a8ca340f0300e7
sauliyo15 <gc.saul.7@gmail.com>
2023-10-31 19:37:10 +0100

    Modelos creados Actor y Pelicula

---
+++ lib/src/models/actor_model.dart
@@ -0,0 +1,96 @@
+//Clase Actores, que sirve para crear la lista de actores
+class Actores {
+  List<Actor> items = [];
+
+  //Constructor
+  Actores();
+
+  //Metodo que permite operar con los datos que provienen de un archivo json
+  Actores.fromjsonList(List<dynamic> jsonList) {
+    //Si el json esta vacio, no hay nada que hacer
+    if (jsonList.isEmpty) return;
+
+    //Recorremos los elementos del archivo json llamando al metodo de la clase actor
+    for (var item in jsonList) {
+      //Tendremos un elemento del json con sus claves-valores con el que llamar al metodo y construir el objeto actor
+      final actor = Actor.fromJsonMap(item);
+
+      //Tras ello se añade el actor a la lista de actores
+      items.add(actor);
+    }
+  }
+}
+
+//Clase Actor con todos los atributos que contendra un actor
+class Actor {
+  late int id;
+  late String nombre;
+  late String nombreOriginal;
+  late int popularidad;
+  late String actorImg;
+  late String actorPapel;
+  late int idPelicula;
+  late dynamic createdAt;
+  late String updatedAt;
+  late String lenguajeOriginal;
+  late String tituloOriginal;
+  late String overview;
+  late String posterImg;
+  late String fecLanzamiento;
+  late String titulo;
+  late String video;
+  late int votosAveraje;
+  late int cantidadVotos;
+
+  //Constructor de la clase
+  Actor({
+    required this.id,
+    required this.nombre,
+    required this.nombreOriginal,
+    required this.popularidad,
+    required this.actorImg,
+    required this.actorPapel,
+    required this.idPelicula,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.lenguajeOriginal,
+    required this.tituloOriginal,
+    required this.overview,
+    required this.posterImg,
+    required this.fecLanzamiento,
+    required this.titulo,
+    required this.video,
+    required this.votosAveraje,
+    required this.cantidadVotos,
+  });
+
+  //Objeto que mapea un elemento de json y a traves de sus claves asigna el valor contenido a los atributos del objeto actor
+  Actor.fromJsonMap(Map<String, dynamic> json) {
+    id = json['id'];
+    nombre = json['nombre'];
+    nombreOriginal = json['nombre_original'];
+    popularidad = json['popularidad'];
+    actorImg = json['actor_img'];
+    actorPapel = json['actor_papel'];
+    idPelicula = json['id_pelicula'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    lenguajeOriginal = json['lenguaje_original'];
+    overview = json['overview'];
+    posterImg = json['poster_img'];
+    fecLanzamiento = json['fec_lanzamiento'];
+    titulo = json['titulo'];
+    video = json['video'];
+    votosAveraje = json['votos_averaje'];
+    cantidadVotos = json['cantidad_votos'];
+  }
+
+  //Validacion del contenido de la variable que corresponde a la fotografia del actor
+  getFoto() {
+    if (actorImg.isEmpty) {
+      return 'https://www.brightlands.com/sites/default/files/2019-12/No%20avater.jpg';
+    } else {
+      return 'https://pelicula.devetechnologies.com/public/$actorImg'; //'http://localhost/pelicula_admin/public/$actorImg'; //uso local
+    }
+  }
+}
---
+++ lib/src/models/pelicula_model.dart
@@ -0,0 +1,90 @@
+//Clase Peliculas, que sirve para crear la lista de peliculas
+class Peliculas {
+  List<Pelicula> items = [];
+
+  //Constructor
+  Peliculas();
+
+  //Metodo que permite operar con los datos que provienen de un archivo json
+  Peliculas.fromjsonList(List<dynamic> jsonList) {
+    //Si el json esta vacio, no hay nada que hacer
+    if (jsonList.isEmpty) return;
+
+    //Recorremos los elementos del archivo json llamando al metodo de la clase pelicula
+    for (var item in jsonList) {
+      //Tendremos un elemento del json con sus claves-valores con el que llamar al metodo y construir el objeto pelicula
+      final pelicula = Pelicula.fromjsonMap(item);
+
+      //Tras ello se añade la pelicula a la lista de peliculas
+      items.add(pelicula);
+    }
+  }
+}
+
+//Clase Pelicula con todos los atributos que contendra una pelicula
+class Pelicula {
+  late int id;
+  late String lenguajeOriginal;
+  late String tituloOriginal;
+  late String overview;
+  late int popularidad;
+  late String posterImg;
+  late String fecLanzamiento;
+  late String titulo;
+  late String video;
+  late int votosAveraje;
+  late int cantidadVotos;
+  late dynamic createdAt;
+  late dynamic updatedAt;
+
+  //Constructor de la clase
+  Pelicula(
+      {required this.id,
+      required this.lenguajeOriginal,
+      required this.tituloOriginal,
+      required this.overview,
+      required this.popularidad,
+      required this.posterImg,
+      required this.fecLanzamiento,
+      required this.titulo,
+      required this.video,
+      required this.votosAveraje,
+      required this.cantidadVotos,
+      required this.createdAt,
+      required this.updatedAt});
+
+  //Objeto que mapea un elemento de json y a traves de sus claves asigna el valor contenido a los atributos del objeto pelicula
+  Pelicula.fromjsonMap(Map<String, dynamic> json) {
+    id = json['id'];
+    lenguajeOriginal = json['lenguaje_original'];
+    tituloOriginal = json['titulo_original'];
+    overview = json['overview'];
+    popularidad = json['popularidad'];
+    posterImg = json['poster_img'];
+    fecLanzamiento = json['fec_lanzamiento'];
+    titulo = json['titulo'];
+    video = json['video'];
+    votosAveraje = json['votos_averaje'];
+    cantidadVotos = json['cantidad_votos'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
+
+  //Validacion del contenido de la variable que corresponde a la imagen de la pelicula
+  getPosterImage() {
+    if (posterImg.isEmpty) {
+      return 'https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg';
+    } else {
+      return 'https://pelicula.devetechnologies.com/public/$posterImg'; //'http://localhost/pelicula_admin/public/$posterImg';
+    }
+  }
+
+  //Validacion del contenido de la variable que corresponde a la imagen de la pelicula
+  getBackgroundImage() {
+    if (posterImg.isEmpty) {
+      return 'https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg';
+    } else {
+      return 'https://pelicula.devetechnologies.com/public/$posterImg'; //'http://localhost/pelicula_admin/public/$posterImg'; uso local
+    }
+  }
+}
