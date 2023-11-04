0b6897d267b994dcafefdcb4b6583e975b47d02e
sauliyo15 <gc.saul.7@gmail.com>
2023-11-04 19:54:59 +0100

    Home Page - Comentado - OK

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,16 +1,27 @@
+// ignore_for_file: prefer_final_fields, avoid_unnecessary_containers, sized_box_for_whitespace, must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
 import 'package:peliculas/src/providers/peliculas_providers.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
+//Clase que lanza la pantalla principal de la aplicacion representanco con un Widget personalizado una lista de peliculas
 class HomePage extends StatelessWidget {
-  PeliculaProvider _peliculas = new PeliculaProvider();
-  final List<Pelicula> pelicula = [];
+  //Encargado de llamar al webservice y rellenar la lista de peliculas
+  PeliculaProvider _peliculas = PeliculaProvider();
+
+  //Constructor
+  HomePage({super.key});
+
+  //Este atributo por el momento no se usa--------------------------------------
+  //final List<Pelicula> pelicula = [];
+
+  //Metodo que construye la pantalla de la aplicacion con sus elementos-widgets
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Movies'),
+        title: const Text('Peliculas Disney'),
       ),
       body: Container(
         child: Column(
@@ -23,6 +34,7 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  //Metodo que devuelve un array de Widgets personalizados con CardSwiper cuyo contenido se obtiene tras llamar al metodo del provider
   Widget _swipeTarjetas() {
     return FutureBuilder(
         future: _peliculas.getCines(),
@@ -34,7 +46,7 @@ class HomePage extends StatelessWidget {
           } else {
             return Container(
                 height: 400.0,
-                child: Center(
+                child: const Center(
                   child: CircularProgressIndicator(),
                 ));
           }
