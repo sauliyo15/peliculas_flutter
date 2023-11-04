c946c87b2bea16a70cc3cc5dcdc6402f94d866d2
sauliyo15 <gc.saul.7@gmail.com>
2023-11-04 13:16:07 +0100

    Routes - Comentado - OK

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/pages/home_page.dart';
 
+//Archivo de rutas que con su metodo devolvera un map con todas las rutas (y metodos/pantallas) indicadas en el return
 Map<String, WidgetBuilder> getRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => HomePage(),
